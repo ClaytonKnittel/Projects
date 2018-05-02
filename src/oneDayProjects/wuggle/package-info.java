02f19be0af5851ad2580aabac51a058a00221757
claytonknittel <claytonknittel@claytons-mbp.dhcp.wustl.edu>
2018-05-01 20:00:04 -0500

    fix

--- src/oneDayProjects/wuggle/package-info.java
+++ src/oneDayProjects/wuggle/package-info.java
@@ -1,6 +1,8 @@
 
 /**
  * WUggle, heavily inspired by the Spring 2018 final project of WashU's CSE 131.
+ * <p>
+ * Completed April 30, 2018
  */
 @OneDay(date = "04/30/2018")
 package oneDayProjects.wuggle;
