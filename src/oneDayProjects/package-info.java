90fd1395f1e34068c99bddc4b0567951ca289076
claytonknittel <claytonknittel@claytons-mbp.dhcp.wustl.edu>
2018-05-01 19:50:24 -0500

    Final commit

---
+++ src/oneDayProjects/package-info.java
@@ -0,0 +1,5 @@
+
+/**
+ * Challenge projects that I commit one and only one day to making from start to finish.
+ */
+package oneDayProjects;
--- src/oneDayProjects/wuggle/package-info.java
+++ src/oneDayProjects/wuggle/package-info.java
@@ -1,6 +1,6 @@
 
 /**
- *
+ * WUggle, heavily inspired by the Spring 2018 final project of WashU's CSE 131.
  */
 @OneDay(date = "04/30/2018")
 package oneDayProjects.wuggle;
